f7e19f8fd7b87089ff085da75d1e1b5b43bf80c1
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-03-08 22:20:26 +0100

    chore: add susoense boundary to locations page

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,12 +1,17 @@
 import Locations from '@/_pages/locations';
 import { Metadata } from 'next';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
 	title: 'Locations',
 };
 
 const Locationspage = () => {
-	return <Locations />;
+	return (
+		<Suspense>
+			<Locations />
+		</Suspense>
+	);
 };
 
 export default Locationspage;
