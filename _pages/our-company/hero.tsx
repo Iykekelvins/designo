dd440378fae84e488666534ffabb9f828757d72b
Kelvin Ochubili <kelvinochubili@gmail.com>
2026-03-11 15:59:51 +0100

    feat: add hero animation for our company page

--- _pages/our-company/hero.tsx
+++ _pages/our-company/hero.tsx
@@ -1,8 +1,129 @@
+'use client';
+
+import { useEffect, useRef } from 'react';
+import { useGSAP } from '@gsap/react';
+import { gsap, SplitText } from '@/lib/gsap';
+
 import Image from 'next/image';
 
 export default function Hero() {
+	const hero = useRef<HTMLElement>(null);
+	const title = useRef<HTMLHeadingElement>(null);
+	const paragraph = useRef<HTMLParagraphElement>(null);
+	const image = useRef<HTMLImageElement>(null);
+	const heroTl = useRef<GSAPTimeline>(null);
+
+	useGSAP(
+		() => {
+			const titleSplit = SplitText.create(title.current, {
+				type: 'lines',
+				mask: 'lines',
+			});
+			const paragraphSplit = SplitText.create(paragraph.current, {
+				type: 'lines',
+				mask: 'lines',
+			});
+
+			gsap.set([title.current, paragraph.current], { opacity: 1 });
+			gsap.set(titleSplit.masks, {
+				perspective: '500px',
+			});
+			gsap.set(titleSplit.lines, {
+				yPercent: 100,
+				rotateY: '-5deg',
+				transformStyle: 'preserve-3d',
+			});
+			gsap.set(paragraphSplit.lines, {
+				yPercent: 100,
+			});
+
+			const mm = gsap.matchMedia();
+
+			mm.add('(min-width: 1200px)', () => {
+				heroTl.current = gsap
+					.timeline({
+						paused: true,
+					})
+					.to(titleSplit.lines, {
+						yPercent: 0,
+						stagger: 0.1,
+						rotateY: '0deg',
+						ease: 'expo.out',
+						duration: 1,
+					})
+					.to(
+						paragraphSplit.lines,
+						{
+							yPercent: 0,
+							ease: 'sine.out',
+							duration: 0.45,
+							stagger: 0.1,
+						},
+						'-=1.2',
+					)
+					.to(
+						image.current,
+						{
+							x: 0,
+							opacity: 1,
+							ease: 'none',
+						},
+						'-=1',
+					);
+			});
+
+			mm.add('(max-width: 1199px)', () => {
+				heroTl.current = gsap
+					.timeline({
+						paused: true,
+						onComplete: () => {
+							titleSplit.revert();
+							paragraphSplit.revert();
+						},
+					})
+					.to(image.current, {
+						y: 0,
+						opacity: 1,
+						ease: 'none',
+					})
+					.to(
+						titleSplit.lines,
+						{
+							yPercent: 0,
+							stagger: 0.1,
+							rotateY: '0deg',
+							ease: 'expo.out',
+							duration: 1,
+						},
+						'-=0.75',
+					)
+					.to(
+						paragraphSplit.lines,
+						{
+							yPercent: 0,
+							ease: 'sine.out',
+							duration: 0.45,
+							stagger: 0.1,
+						},
+						'<',
+					);
+			});
+		},
+		{
+			scope: hero,
+		},
+	);
+
+	useEffect(() => {
+		if (hero.current) {
+			heroTl.current?.play();
+		}
+	}, []);
+
 	return (
-		<section className='bg-peach sm:rounded-15 overflow-hidden -mx-gutter sm:mx-0'>
+		<section
+			className='bg-peach sm:rounded-15 overflow-hidden -mx-gutter sm:mx-0'
+			ref={hero}>
 			<div className='flex flex-col-reverse des:flex-row justify-between'>
 				<div
 					className='flex flex-col justify-center flex-1 
@@ -13,11 +134,14 @@ export default function Hero() {
 						className='text-center des:text-left py-[max(5rem,80px)]
 						px-gutter sm:py-[max(4rem,64px)] des:py-0 relative z-2
 						'>
-						<h1 className='text-48 font-medium'>About Us</h1>
+						<h1 className='text-48 font-medium opacity-0' ref={title}>
+							About Us
+						</h1>
 						<p
 							className='text-16 leading-[1.6] max-w-[max(551px)] 
 							des:max-w-[max(28.625rem,327px)] 
-							mt-[max(2rem,24px)]'>
+							mt-[max(2rem,24px)] opacity-0'
+							ref={paragraph}>
 							Founded in 2010, we are a creative agency that produces lasting results
 							for our clients. We’ve partnered with many startups, corporations, and
 							nonprofits alike to craft designs that make real impact. We’re always
@@ -85,7 +209,11 @@ export default function Hero() {
 						</defs>
 					</svg>
 				</div>
-				<figure className='relative z-2'>
+				<figure
+					className='relative z-2 -translate-y-[15%] des:translate-x-[15%] opacity-0
+					h-full des:translate-y-0
+					'
+					ref={image}>
 					<Image
 						src='/images/about/image-about-hero.webp'
 						width={476}
